4ce6ba1a37b4480dde358a6778d8621c0784efc7
BocMaN77703 <6491414@mail.ru>
2020-10-17 14:53:01 +0300

    count of elements

---
+++ LAB7.cpp
@@ -0,0 +1,48 @@
+﻿//num 4
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int** ms;
+	int s[3] = {0,0,0}, n = 0, i = 0, j = 0;
+	printf_s("Введите кол-во строк и столбцов: ");
+	scanf_s("%d", &n);
+	ms = (int**)malloc(n * sizeof(int));
+	for (i = 0; i < n; i++)
+		ms[i] = (int*)malloc(n * sizeof(int));
+	printf_s("Введите элементы:\n");
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			if (!scanf_s("%d", &ms[i][j])) {
+				printf_s("Вы ввели не число. Введите элементы:\n");
+				rewind(stdin);
+			}
+		}
+	}
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) {
+		if ((ms[i][0] < 0) && (i < j)){
+			s[0] += ms[i][j];
+		}
+	}
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) {
+		if ((ms[i][0] < 0) && (i == j)) {
+			s[1] += ms[i][j];
+		}
+	}
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) {
+		if ((ms[i][0] < 0) && (i > j)) {
+			s[2] += ms[i][j];
+		}
+	}
+	printf_s("Сумма элементов выше главной диагонали: %d\n", s[0]);
+	printf_s("Сумма элементов на главной диагонали: %d\n", s[1]);
+	printf_s("Сумма элементов ниже главной диагонали: %d\n", s[2]);
+	for (i = 0; i < n; i++)
+		free(ms[i]);
+	free(ms);
+	return 0;
+}
