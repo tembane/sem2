325ddf813ca3acf069e4d61ca5bb35a94f683b61
BocMaN77703 <6491414@mail.ru>
2020-11-12 20:17:47 +0300

    stroka

---
+++ LAB8/LAB8.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	int n;
+	char *s;
+	setlocale(LC_ALL, "RUS");
+	printf("Введите кол-во элементов строки: ");
+	while(!scanf_s("%d", &n))
+	{
+		printf("Ошибка ввода. Введите число:");
+		rewind(stdin);
+	}
+	n++;
+	// Выделение памяти
+	s = (char*)malloc(n * sizeof(char));
+	printf("Введите элементы строки: ");
+	// Ввод строки
+	for (int i = 0; i < n; i++)
+	{
+		s[i]=getchar();
+	}
+	printf("Элементы между двоеточиями:\n");
+	// Отбор элементов между двумя двоеточиями
+	for (int i = 0; i < n; i++)
+	{
+		if (s[i] == 58) 
+		{
+			i++;
+			for (i; i < n; i++)
+				if (s[i] != 58)
+				{
+					printf("%c", s[i]);
+				}
+				else
+				{
+					i = n;
+				}
+		}
+	}
+	// Очищение памяти
+	free(s);
+	return 0;
+}
