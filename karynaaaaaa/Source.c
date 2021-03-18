aceea0f255460cee409fb67b64b51521c4e9d342
tembane <74444812+tembane@users.noreply.github.com>
2021-03-18 21:23:04 +0300

    Update Source.c

--- karynaaaaaa/Source.c
+++ karynaaaaaa/Source.c
@@ -85,7 +85,7 @@ void hat() {
 	printf("|\t%-2s\t|\t%-15s\t|\t%-8s\t|\t%-9s\t|\t%-s\t\t\t|\n", "#", "Name", "Price", "Genre", "Max Players");
 	line();
 }
-void printWorker(struct TheGame* games, int i) {
+void printGames(struct TheGame* games, int i) {
 	if (games[i].flag == 1)
 		printf("|\t%-2d\t|\t%-20s\t|\t%-.2f\t|\t%-20s\t|\t%-s\t\t|\n", i + 1,
 			games[i].name, games[i].price, games[i].rules.gameGenre, "-");
@@ -100,4 +100,191 @@ void line()
 		printf("%c", '-');
 	}
 	printf("\n");
+}
+
+void change(struct TheGame* games, int arraySize)
+{
+    printf("Choose an operation:\n");
+    printf("1)  Edit the entire post\n");
+    printf("2)  Edit part of note\n");
+    printf("3)  Exit\n");
+    int choice = correctChoice(3);
+    switch (choice)
+    {
+        case 1:
+            changeOneGame(games, arraySize);
+            break;
+        case 2:
+            partChange(games, arraySize);
+            break;
+        case 3:
+            break;
+    }
+
+}
+void changeOneGame(struct TheGame* pointer, int arraySize)
+{
+    printf("Enter game number\n");
+    int gameNumber = correctChoice(arraySize);
+    gameNumber--;
+
+    printf("Enter new game name\n");
+    rewind(stdin);
+    gets(pointer[gameNumber].name);
+
+    printf("Enter new price size\n");
+    pointer[gameNumber].price = inputValidationFloat();
+
+    if (pointer[gameNumber].flag == 1)
+    {
+        printf("Make a choice\n");
+        printf("1)  Change game genre\n");
+        printf("2)  Change the field with the game genre to the maximum number of players\n");
+        int choice = correctChoice(2);
+        switch (choice)
+        {
+            case 1:
+                printf("Enter a new genre\n");
+                rewind(stdin);
+                gets(pointer[gameNumber].rules.gameGenre);
+                //break;
+                return;
+            case 2:
+                printf("Enter the maximum number of players\n");
+                pointer[gameNumber].rules.maxPlayers = inputValidationInt();
+                pointer[gameNumber].flag = 2;
+                //break;
+                return;
+        }
+    }
+    if (pointer[gameNumber].flag == 2)
+    {
+        printf("Make choice\n");
+        printf("1)  Change the maximum number of players\n");
+        printf("2)  Change the maximum number of players to the game genre\n");
+        int choice = correctChoice(2);
+        switch (choice)
+        {
+            case 1:
+                printf("Enter the maximum number of players\n");
+                pointer[gameNumber].rules.maxPlayers = inputValidationInt();
+                //break;
+                return;
+            case 2:
+                printf("Enter new genre\n");
+                rewind(stdin);
+                gets(pointer[gameNumber].rules.gameGenre);
+                pointer[gameNumber].flag = 1;
+                //break;
+                return;
+        }
+    }
+}
+void partChange(struct TheGame* pointer, int arraySize)
+{
+    printf("Enter game number\n");
+    int gameNumber = correctChoice(arraySize);
+    gameNumber--;
+    printf("Make a choice\n");
+    printf("1)  Change game name\n");
+    printf("2)  Change price size\n");
+    printf("3)  Change game genre\n");
+    printf("4)  Change the maximum number of players\n");
+    int choice = correctChoice(4);
+    switch (choice)
+    {
+        case 1:
+            printf("Enter new game name\n");
+            rewind(stdin);
+            gets(pointer[gameNumber].name);
+            break;
+        case 2:
+            printf("Enter new price size\n");
+            pointer[gameNumber].price = inputValidationFloat();
+            break;
+        case 3:
+            if (pointer[gameNumber].flag == 1)
+            {
+                printf("Enter the new genre\n");
+                rewind(stdin);
+                gets(pointer[gameNumber].rules.gameGenre);
+            }
+            if (pointer[gameNumber].flag == 2)
+            {
+                printf("If you change this field,"
+                       "you will no longer be able to store information about the maximum number of players\n");
+                printf("Do you want to make changes?\n");
+                printf("1)No\n2)Yes\n");
+                int editChoice = correctChoice(2);
+                switch (editChoice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        printf("Enter the genre\n");
+                        rewind(stdin);
+                        gets(pointer[gameNumber].rules.gameGenre);
+                        pointer[gameNumber].flag = 1;
+                }
+            }
+            break;
+        case 4:
+            if (pointer[gameNumber].flag == 2)
+            {
+                printf("Enter the maximum number of players\n");
+                pointer[gameNumber].rules.maxPlayers = inputValidationInt();
+            }
+            if (pointer[gameNumber].flag == 1)
+            {
+                printf("If you change this field,"
+                       "you will no longer be able to store information about the genre of the game\n");
+                printf("Do you want to make changes?\n");
+                printf("1)No\n2)Yes\n");
+                int editChoice = correctChoice(2);
+                switch (editChoice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        printf("Enter the maximum number of players\n");
+                        pointer[gameNumber].rules.maxPlayers = inputValidationInt();
+                        pointer[gameNumber].flag = 2;
+                }
+            }
+            break;
+    }
+}
+
+
+
+//------------Проверки на ввод------------//
+int inputValidationInt()
+{
+    int n;
+    while (scanf("%d", &n) != 1 || n <= 0)
+    {
+        rewind(stdin);
+        printf("Try again\n");
+    }
+    return n;
+}
+float inputValidationFloat()
+{
+    float n;
+    while (scanf("%f", &n) != 1 || n <= 0)
+    {
+        rewind(stdin);
+        printf("Try again\n");
+    }
+    return n;
+}
+int correctChoice(int k)
+{
+    int n;
+    while (scanf("%d", &n) != 1 || n <= 0 || n > k)
+    {
+        rewind(stdin);
+        printf("Try again\n");
+    }
+    return n;
 }
