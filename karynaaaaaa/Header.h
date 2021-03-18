5c856908699029d9cdfeb3449b505c1253e7acd9
tembane <74444812+tembane@users.noreply.github.com>
2021-03-18 21:25:38 +0300

    Update Header.h

--- karynaaaaaa/Header.h
+++ karynaaaaaa/Header.h
@@ -1,7 +1,13 @@
-int InputValidation();
-void line();
-void printWorker(struct TheGame*, int);
-void hat();
-void show(struct TheGame*, int );
 struct TheGame* add(struct TheGame*, int, int);
+void show(struct TheGame* games, int numberOfGames);
+void printGames(struct TheGame* games, int i);
+
+void change(struct TheGame * pointer, int arraySize);
+void changeOneGame (struct TheGame *pointer, int arraySize);
+void partChange(struct TheGame *pointer, int arraySize);
+int inputValidationInt();
+float inputValidationFloat();
+void line();
+void hat();
 int menu();
+int correctChoice(int k);
