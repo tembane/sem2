0929ac0d5b9850e730d250275225a7dc9f62477d
tembane <74444812+tembane@users.noreply.github.com>
2021-03-18 21:19:01 +0300

    Update main.c

--- karynaaaaaa/main.c
+++ karynaaaaaa/main.c
@@ -11,25 +11,36 @@ int main()
 		switch (menu())
 		{
 		case 1:
-			printf("Number of games you want to add: ");
-			while (!scanf_s("%d", &newNumberOfGames) || (newNumberOfGames <= 0))
-			{
-				rewind(stdin);
-				printf("Incorrect value.");
-			}
-			numberOfGames += newNumberOfGames;
-			games = add(games, numberOfGames, newNumberOfGames);
-			break;
-		case 2: show(games, numberOfGames); break;
-		case 3:
-			//games = del(games, numberOfGames);
-			break;
-		case 4:	//search(games, numberOfGames); break;
-		case 5: //sort(games, numberOfGames); break;
-		case 6: //games = change(games, numberOfGames); break;
-		case 7: //partSearch(games, numberOfGames); break;
-		case 8:
-			free(games);
+                printf("Number of games you want to add: \n");
+                /*while (!scanf("%d", &newNumberOfGames) || (newNumberOfGames <= 0))
+                {
+                    rewind(stdin);
+                    printf("Incorrect value.");
+                }*/
+                newNumberOfGames = inputValidationInt();
+                numberOfGames += newNumberOfGames;
+                games = add(games, numberOfGames, newNumberOfGames);
+                break;
+            case 2:
+                show(games, numberOfGames);
+                break;
+            case 3:
+                change(games, numberOfGames);
+                break;
+            case 4:
+                //search(games, numberOfGames);
+                // break;
+            case 5:
+                //sort(games, numberOfGames);
+                // break;
+            case 6:
+
+                break;
+            case 7:
+                //partSearch(games, numberOfGames);
+                // break;
+            case 8:
+                free(games);
 			return 0;
 		default:
 			printf("Error. Incorrect value!\n");
